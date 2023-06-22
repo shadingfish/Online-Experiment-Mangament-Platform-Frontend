de7bfdcb530dab85aec7a48a6be2c3f0f8998d13
yudingyi <yudingyi054@gmail.com>
2023-06-22 14:38:43 +0800

    修改

--- src/api/exp.js
+++ src/api/exp.js
@@ -1,19 +1,76 @@
 import request from '../util/request.js'
 
-export function getParticipant(expId) {
-    const data = {
-        expId: expId
-    };
+export function getExpRec() {
     return request({
-        url: '/experiment/getParticipant',
-        method: 'get',
-        params: data
+      url: '/experiment/getlist',
+      method: 'get',
+    })
+}
+//根据用户ID获得我参与的实验列表
+export function getJoinedExpList() {
+  return request({
+    url: '/distribution/joinedByList',
+    method: 'get',
+  })
+}
+export function getParticipantByExpId(expId) {
+  const data={
+    expId: expId
+  }
+  return request({
+    url: '/experiment/getParticipant',
+    method: 'get',
+    params:data
+  })
+}
+
+export function updateExpRec(fileinfo) {
+    const data=fileinfo
+    return request({
+      url: '/experiment/update',
+      method: 'post',
+      data: data
+    })
+}
+
+export function removeExpRec(fileinfo) {
+    const data=fileinfo
+    return request({
+      url: '/experiment/delete',
+      method: 'delete',
+      data: data
+    })
+}
+
+export function getExpRecById(userid) {
+  return request({
+    url: '/experiment/delete',
+    method: 'delete',
+    params: userid
+  })
+}
+
+//删除实验
+export function deleteExperiment(expId) {
+    return request({
+        url: `/experiment/delete?expId=${expId}`,
+        method: 'post'
     });
 }
-// 获取用户信息
-export function getInfo() {
+
+//查看被试名单
+export function getParticipant(expId) {
     return request({
-        url: '/admin-api/user/profile/get',
+        url:`/experiment/getParticipant?expId=${expId}`,
         method: 'get'
     })
 }
+
+//删除被试者
+export function deleteParticipant(expId, username) {
+    return request({
+        url:`/experiment/deleteParticipant?expId=${expId}&username=${username}`,
+        method: 'post'
+    })
+}
+
